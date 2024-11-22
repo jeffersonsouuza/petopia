caac3fd929c8da53b4a163ff6d1dc93a260bf7db
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-11-22 14:26:01 -0300

    improve: definir as cores no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './src/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-100': '#FFFFFF',
+        'white-200': '#F5F5F5',
+        'white-300': 'rgba(255, 255, 255, 0.7)',
+        'muted-100': '#E0E8E0',
+        'muted-200': '#8C8C8C',
+        'green-100': '#1C4A2A',
+        'green-200': '#0E2515',
+      },
+    },
   },
   plugins: [],
 };
